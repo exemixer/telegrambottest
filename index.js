dea11efad8bf9e56123eaaa643d8fbe0391bb637
Mike Azizov <azizov.mike@gmail.com>
2019-02-05 14:08:30 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const TelegramBot = require('node-telegram-bot-api')
+
+
+const TOKEN ='701183305:AAFabVlO8Mo8oPSKAc3EUb-PvP3sZRLIGc0'
+
+const bot = new TelegramBot(TOKEN, {polling:true})
+
+bot.on('messege', msg =>{
+	bot.sendMessage(msi.chat.id, 'привет, bot says: "Привет ${msg.from.first_name}"')
+})
+
